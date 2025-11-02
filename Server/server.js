d0c29e51c88e6be146e27129e1426ac22f1e5d6d
gluk <nek.art.13@gmail.com>
2025-11-02 17:49:08 +0300

    fix:(server.js): изменил запуск сервера

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,5 @@
-import express from 'express';
+const PORT = process.env.PORT || 4200;
 
-const app = express();
-
-app.use(express.static('../client/dist'));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
 });
